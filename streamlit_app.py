99e44e148146e1cb90ad53d1b62c8a66027e5fbd
Olivia Lara <oliviamarielara@gmail.com>
2025-02-12 21:04:16 +0000

    fix chatbot url

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.subheader("AI Chatbot: Policy Proposal Help")
 st.write("[Fulcrum](https://www.withfulcrum.com/) gives AI-Powered products to insurance brokerages. They give clients \
           the ability ask questions on generated and active policies. Inspired by such work, I have created a simple chatbot \
          built to help users understand commercial policy proposals and ask questions on it.")
-st.write('See chatbot here: [Policy Assistant Chatbot][https://chatgpt.com/g/g-67acfa0e67ac8191b09659322b76c2d0-policy-assistant]')
+st.write('See chatbot here: [Policy Assistant](https://chatgpt.com/g/g-67acfa0e67ac8191b09659322b76c2d0-policy-assistant)')
 
 # # Initialize chat history
 # if "messages" not in st.session_state:
